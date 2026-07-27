727bf8513926bae7a2676f17844ca14ee281b4d5
solaimanalideveloper <solaimanalideveloper@gmail.com>
2026-07-27 15:08:06 +0600

    Chack if a String in a Plaingdom

--- problem-7.js
+++ problem-7.js
@@ -1,1 +1,37 @@
-// problem-7 : 
+// problem-7 : Chack if a String in a Plaingdom
+
+// Without Functon
+
+// let str = "dad";
+// let reversStr = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//   let letter = str[i];
+//   reversStr += letter;
+// }
+// console.log(reversStr);
+
+// if (str === reversStr) {
+//   console.log("Plaingdom Word");
+// } else {
+//   console.log("Not a Plaingdom Word");
+// }
+
+// With Function
+
+function isPlaingdom(str) {
+  let reverseStr = "";
+  
+  for (let i = str.length - 1; i >= 0; i--) {
+    let letter = str[i];
+    reverseStr += letter;
+  }
+  console.log(reverseStr);
+  if (str === reverseStr) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPlaingdom("madam"));
