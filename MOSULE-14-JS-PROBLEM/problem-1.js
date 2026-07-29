284df35a4d8c11885147d90bf318ec93136971e2
solaimanalideveloper <solaimanalideveloper@gmail.com>
2026-07-29 18:51:27 +0600

    Spot the Error Type

---
+++ MOSULE-14-JS-PROBLEM/problem-1.js
@@ -0,0 +1,34 @@
+// 1.
+// function sayHi(name) {
+//   console.log("Hi, " + name )
+// }
+// sayHi("Mira");
+
+// 2.
+// let word = "hello";
+// let touppercase = word.toUpperCase();
+// console.log(touppercase);
+
+// 3.
+// function square(num) {
+//   return num ** 2; // find the bug
+// }
+// console.log(square(4)); // should be 16
+
+// 4.
+// Bug type: ___
+function sayHi(name) {
+  console.log("Hi, " + name);
+}
+sayHi("Mira");
+
+// Bug type: ___
+let word = "hello";
+let words = word.toUpperCase();
+console.log(words);
+
+// Bug type: ___
+function square(num) {
+  return num ** 2;
+}
+console.log(square(4));
