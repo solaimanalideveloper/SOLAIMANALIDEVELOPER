bd7057310a00e5733a6b2c5aa7cd6b599482947f
solaimanalideveloper <solaimanalideveloper@gmail.com>
2026-08-03 07:06:57 +0600

    ES6 Practice Tasks

---
+++ Module-16-"ES6"-JS/default-value.js
@@ -0,0 +1,19 @@
+// function greed(name) {
+//   return "Hello " + name;
+// }
+// console.log(greed("Solaiman"));
+
+function cractor(name = "Geast", roll = "Mentor") {
+  return "Hi iam " + name + ". Progmming Hero in a " + roll;
+}
+console.log(cractor());
+
+function cractor(name = "Geast", roll = "Mentor") {
+  return "Hi iam " + name + ". Progmming Hero in a " + roll;
+}
+console.log(cractor("Solaiman","Student"));
+
+// function greed(roll="geast") {
+//   return `$ Solaiman is  {roll}`;
+// }
+// console.log(greed("student"));
