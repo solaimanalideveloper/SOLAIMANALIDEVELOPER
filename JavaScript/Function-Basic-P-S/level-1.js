48971e45b6deb745ae6ae1a2e9f8c0abd0810740
solaimanalideveloper <solaimanalideveloper@gmail.com>
2026-09-18 11:36:14 +0600

    Function basic problem solved in javascript

---
+++ JavaScript/Function-Basic-P-S/level-1.js
@@ -0,0 +1,170 @@
+// function add(a, b) {
+//   return a + b;
+// }
+// console.log(add(10, 20));
+
+// function checkEvenOdd(number) {
+//   if (number % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// }
+// console.log(checkEvenOdd(21));
+
+// const findMax = (a, b) => {
+//   if (a > b) {
+//     return `A is the big number: ${a}`;
+//   } else {
+//     return `B is the big number: ${b}`;
+//   }
+// };
+// console.log(findMax(122, 111));
+
+// const greet = (name) => {
+//   return `Hello ${name}`;
+// };
+// console.log(greet("Solaiman"));
+// console.log(greet("Moriom"));
+
+// const square = (number) => {
+//   return number * number;
+// };
+// console.log(square(5));
+
+// const checkNumber = (number) => {
+//   if (number > 0) {
+//     return "Positive";
+//   } else if (number < 0) {
+//     return "Negative";
+//   } else {
+//     return "Zero";
+//   }
+// };
+// console.log(checkNumber(10));
+// console.log(checkNumber(-5));
+// console.log(checkNumber(0));
+
+// const sumThree = (a, b, c) => {
+//   return a + b + c;
+// };
+// console.log(sumThree(10, 20, 30));
+// console.log(sumThree(5, 15, 20));
+
+// const checkAge = (age) => {
+//   if (age >= 18) {
+//     return "Adult";
+//   } else {
+//     return "Child";
+//   }
+// };
+// console.log(checkAge(20));
+// console.log(checkAge(15));
+
+// const multiply = (a, b) => {
+//   return a * b;
+// };
+// console.log(multiply(5, 4));
+// console.log(multiply(7, 3));
+
+// const findLarge = (a, b) => {
+//   if (a > b) {
+//     return `A is the large: ${a}`;
+//   } else {
+//     return `B is the large: ${b}`;
+//   }
+// };
+// console.log(findLarge(20, 35));
+// console.log(findLarge(50, 10));
+
+// const fullName = (firstName, lastName) => {
+//   return `${firstName} ${lastName}`;
+// };
+// console.log(fullName("Solaiman", "All"));
+// console.log(fullName("Rahim", "Ahmed"));
+
+// const birthYear = (age) => {
+//   return 2026 - age;
+// };
+// console.log(birthYear(20));
+// console.log(birthYear(25));
+
+// const checkEven = (number) => {
+//   if (number % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// };
+// console.log(checkEven(12));
+// console.log(checkEven(9));
+
+// const checkNumber = (number) => {
+//   if (number > 0 && number % 2 === 0) {
+//     return "Positive Even";
+//   } else if (number > 0 && number % 2 === 1) {
+//     return "Positive Odd";
+//   } else if (number < 0) {
+//     return "Negative";
+//   } else {
+//     return "Zero";
+//   }
+// };
+// console.log(checkNumber(10));
+// console.log(checkNumber(7));
+// console.log(checkNumber(-4));
+// console.log(checkNumber(0));
+
+// const checkResult = (marks) => {
+//   if (marks >= 40) {
+//     return "Pass";
+//   } else {
+//     return "Fall";
+//   }
+// };
+// console.log(checkResult(75));
+// console.log(checkResult(35));
+
+// const discountPrice = (taka) => {
+//   if (taka >= 2000) {
+//     let discountTaka = (taka * 10) / 100;
+//     let discountAmount = taka - discountTaka;
+//     return discountAmount;
+//   } else {
+//     return taka;
+//   }
+// };
+// console.log(discountPrice(2000));
+// console.log(discountPrice(800));
+// console.log(discountPrice(3200));
+
+// const findMax = (a, b, c) => {
+//   if (a >= b && a >= b) {
+//     return a;
+//   } else if (b >= a && b >= c) {
+//     return b;
+//   } else {
+//     return c;
+//   }
+// };
+// console.log(findMax(10, 50, 30));
+// console.log(findMax(100, 40, 80));
+
+// const login = (userName, password) => {
+//   if (userName === "admin" && password === "1234") {
+//     return "Login Successful";
+//   } else {
+//     return "Invalid UserName or Password";
+//   }
+// };
+// console.log(login("admin", "1234"));
+// console.log(login("admin", "1111"));
+
+// const celsiusToFahrenheit = (c) => {
+//   let f = (c * 9) / 5 + 32;
+//   return f;
+// };
+// console.log(celsiusToFahrenheit(0));
+// console.log(celsiusToFahrenheit(100));
+
+
